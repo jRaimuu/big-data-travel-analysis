11105ad3096f6d11d47b6aad967262b2c132457e
pavobravo11 <gustavoa1011@gmail.com>
2024-12-12 14:07:45 -0700

    Updated the clean data table files

--- pySpark/clean_tables.py
+++ pySpark/clean_tables.py
@@ -4,49 +4,40 @@ from pyspark.sql.functions import col, trim
 
 from dotenv import load_dotenv
 from gcsfs import GCSFileSystem
-from os import getenv
-
-
-def load_bucket_files(bucket):
-    """Takes a bucket name and loads all the files in a google cloud bucket"""
-    return GCSFileSystem().ls(bucket)
-
-
-def process_file(file_path):
-    """Takes a table based csv file and cleans the string based columns"""
-    print(f"Processing file: {file_path}")
-    df = spark.read.csv(f"gs://{file_path}", header=True, inferSchema=True)
-    
-    for column_name, column_type in df.dtypes:
-        if column_type == "string":
-            df = df.withColumn(column_name, trim(col(column_name)))
-
-    """Writing the file to an output would go here (df.write.csv)"""
-    df.take(3) # Only take the top 3 rows for now
-    print(f"Processed and saved cleaned file: {file_path}")
+from os import getenv, path
+from dotenv import load_dotenv
 
 
 def clean_data_tables():
     """Clean all string columns in bucket by iterating over all csv files"""
-    spark = SparkSession.builder\
-                        .appName("PublicGCSRead")\
+    dotenv_path = path.abspath(path.join(path.dirname(__file__), '../..', '.env'))
+    load_dotenv(dotenv_path)
+    bucket = getenv('BUCKET_NAME')
+
+    spark_csv = SparkSession.builder\
+                        .appName("Clean DSV Data")\
                         .config("spark.jars", "https://storage.googleapis.com/hadoop-lib/gcs/gcs-connector-hadoop3-latest.jar")\
                         .getOrCreate()
 
-    bucket = getenv('BUCKET_NAME')
-
     # Load all files
-    files = load_bucket_files(bucket)
-    csv_files = [file for file in files if file.endswith(".csv")]
+    files = GCSFileSystem().ls(f"gs://{bucket}/")
+    csv_files_paths = [f"gs://{file}" for file in files if file.endswith(".csv")]
 
-    if not csv_files:
+    if not csv_files_paths:
         raise ValueError("No CSV files found in the bucket.")
+    
+    print("Succesfully read files:")
+    for i, name in enumerate(csv_files_paths):
+        print(f"{i}. {name}")
 
-    # Parallelizes the files and processes them
-    rdd_files = spark.sparkContext.parallelize(csv_files)
-    rdd_files.foreach(process_files)
+    # Passing a list to read.csv reads them in parallel
+    df = spark_csv.read.csv(csv_files_paths, header=True, inferSchema=True)
 
-    print("All files processed successfully")
+    for column_name, column_type in df.dtypes:
+        if column_type == "string":
+            df = df.withColumn(column_name, trim(col(column_name)))
+
+    print(df.take(2))
 
 if __name__ == '__main__':
     clean_data_tables()
