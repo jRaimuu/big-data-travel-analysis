8de73bbdda3e2a72ffce5a14067de71ace9280a4
pavobravo11 <gustavoa1011@gmail.com>
2024-12-13 23:23:26 -0700

    Tables can be exported and script runs, but the tables are not perfect yet

--- airflow-server/src/pySpark/clean_tables.py
+++ airflow-server/src/pySpark/clean_tables.py
@@ -1,6 +1,6 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StringType
-from pyspark.sql.functions import month, col
+from pyspark.sql.functions import month, col, sum, when
 
 from dotenv import load_dotenv
 from os import getenv, path
@@ -13,7 +13,6 @@ spark-submit \
     --conf spark.hadoop.fs.gs.impl=com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem \
     clean_tables.py
 '''
-
 
 def clean_data_tables():
     """Clean all string columns in bucket by iterating over all csv files"""
@@ -47,28 +46,29 @@ def clean_data_tables():
     natural_disaster_death = spark.read.csv(f"gs://{bucket}/natural_disaster_death.csv", header=True, inferSchema=True)
     international_tourist_trips = spark.read.csv(f"gs://{bucket}/international_tourist_trips.csv", header=True, inferSchema=True)
 
-    disease_death = spark.read.csv(f"gs://{bucket}/disease_death.csv")
+    disease_death = spark.read.csv(f"gs://{bucket}/disease_death.csv", header=True, inferSchema=True)
     disease_death_renamed = disease_death.withColumnRenamed('location_name', 'name')
-    # disease_death_replaced = disease_death_renamed.replace('United States of America', 'United States', 'name')
+    disease_death_replaced = disease_death_renamed.withColumn(
+        "name",
+        when(col("name") == "United States of America", "United States").otherwise(col("name")))
 
     # Aggregated disease_death
-    # disease_death_year = disease_death_renamed.groupBy(['name', 'year']).agg(sum('val').alias('disease_death_val'))
+    disease_death_year = disease_death_replaced.groupBy(['name', 'year']).agg(sum('val').alias('disease_death_val'))
 
     internet_penetration = spark.read.csv(f"gs://{bucket}/internet_penetration_rate.csv", header=True, inferSchema=True)
     internet_penetration = internet_penetration\
         .withColumnRenamed('it_net_user_zs', 'internet_user_score')\
-        .withColumnRenamed('entity', 'name')\
-        .withColumnRenamed('Year', 'year')
     
     # Monthly surface temp data
     average_monthly_surface_temp_full = spark.read.csv(f"gs://{bucket}/average_monthly_surface_temp.csv", header=True, inferSchema=True)
     average_monthly_surface_temp = average_monthly_surface_temp_full.select('Entity', 'Code', 'year', 'temperature_2m')
-    average_monthly_surface_temp_renamed = average_monthly_surface_temp.withColumnRenamed('Entity', 'name')
+    average_monthly_surface_temp_renamed = average_monthly_surface_temp\
+        .withColumnRenamed('Entity', 'name')\
+        .withColumnRenamed('Code', 'country_code')\
+        .withColumnRenamed('temperature_2m', 'surface_temp')
 
     # Aggregated surface temp data by average
     yearly_surface_temp_full = average_monthly_surface_temp_full.filter(month(average_monthly_surface_temp_full.Day) == 1)
-    yearly_surface_temp_full.printSchema()
-    yearly_surface_temp_full.show(5)
     yearly_surface_temp = yearly_surface_temp_full.select(
         col("Entity"),
         col("Code"),
@@ -79,25 +79,27 @@ def clean_data_tables():
     # Data that is not yearly
     world_heritage_sites = spark.read.csv(f"gs://{bucket}/unesco_whs_by_country.csv", header=True, inferSchema=True)
     world_heritage_sites_renamed = world_heritage_sites\
-        .withColumnRenamed('Total sites', 'heritage_sites')\
-        .withColumnRenamed('Country', 'name')
+        .withColumnRenamed('Total sites', 'heritage_site_count')
+    world_heritage_sites_selected = world_heritage_sites_renamed.select('heritage_site_count', 'Country')
 
-    infrastructure = spark.read.csv(f"gs://{bucket}/infrastructure.csv", header=True, inferSchema=True)
-    infrastructure_selected = infrastructure.select('Country', 'Overall Infrastructure Score', 'Basic Infrastructure Score', \
+    infrastructure_raw = spark.read.csv(f"gs://{bucket}/infrastructure.csv", header=True, inferSchema=True)
+    infrastructure_selected = infrastructure_raw.select('Country', 'Overall Infrastructure Score', 'Basic Infrastructure Score', \
         'Technological Infrastructure Score', 'Scientific Infrastructure Score', 'Health and Environment Score', 'Education Score')
-    
+
     # Make all the columns snake case
-    for col_name in infrastructure_selected:
-        infrastructure = infrastructure_selected.withColumnRenamed(col_name.lower().replace(' ', '_'))
+    for col_name in infrastructure_selected.columns:
+        infrastructure_selected = infrastructure_selected.withColumnRenamed(col_name, col_name.lower().replace(' ', '_'))
 
+    # infrastructure_selected.show(5)
+    # world_heritage_sites_selected.show(5)
 
-    # The data will be grouped into the following tables:
-    # Climate by year (Includes aggregates)
-    # Health and Economy by year (Includes aggregates)
-    # Tourism by year
-    # Monthly rainfall (Not aggregated)
-    # Disease death (Not aggregated)
-    # Infrastructure
+    ## Aggregate the data    
+    cultural = infrastructure_selected\
+        .join(
+            world_heritage_sites_selected,
+            world_heritage_sites_selected.Country == infrastructure_selected.country,
+            'outer')\
+        # .drop(world_heritage_sites_selected.Country)
 
     climate_year = annual_co2 \
             .join(annual_ghe, (annual_co2.Entity == annual_ghe.Entity) & (annual_co2.Year == annual_ghe.Year), 'left')\
@@ -113,41 +115,44 @@ def clean_data_tables():
             .join(
                 yearly_surface_temp, 
                 (annual_co2.Entity == yearly_surface_temp.Entity) \
-                    & (annual_co2.Year == yearly_surface_temp.Year), 
+                    & (annual_co2.Year == yearly_surface_temp.year), 
                 'left')\
             .drop(
                 yearly_surface_temp.Entity, 
-                yearly_surface_temp.Year, 
+                yearly_surface_temp.year, 
                 yearly_surface_temp.Code)
 
-
-    health_econ_year = crime_rate\
+    health_econ_year = gdp_ppp_per_capita\
             .join(
-                gdp_ppp_per_capita, 
-                (crime_rate.Entity == gdp_ppp_per_capita.Entity) & (crime_rate.Year == gdp_ppp_per_capita.Year), 
-                'right')\
-            .drop(gdp_ppp_per_capita.Entity, gdp_ppp_per_capita.Year, gdp_ppp_per_capita.Code) \
+                crime_rate,
+                (gdp_ppp_per_capita.Entity == crime_rate.Entity) & (gdp_ppp_per_capita.Year == crime_rate.Year), 
+                'left')\
+            .drop(crime_rate.Entity, crime_rate.Year, crime_rate.Code) \
             .join(
                 gdp_nominal_per_capita, 
-                (crime_rate.Entity == gdp_nominal_per_capita.Entity) & (crime_rate.Year == gdp_nominal_per_capita.Year), 
+                (gdp_ppp_per_capita.Entity == gdp_nominal_per_capita.Entity) & (gdp_ppp_per_capita.Year == gdp_nominal_per_capita.Year),
                 'left')\
             .drop(gdp_nominal_per_capita.Entity, gdp_nominal_per_capita.Year, gdp_nominal_per_capita.Code) \
-            .join(inflation_rate, (crime_rate.Entity == inflation_rate.Entity) & (crime_rate.Year == inflation_rate.Year), 'left')\
+            .join(
+                inflation_rate, 
+                (gdp_ppp_per_capita.Entity == inflation_rate.Entity) & (gdp_ppp_per_capita.Year == inflation_rate.Year),
+                'left')\
             .drop(inflation_rate.Entity, inflation_rate.Year, inflation_rate.Code) \
             .join(
                 natural_disaster_death, 
-                (crime_rate.Entity == natural_disaster_death.Entity) & \
-                    (crime_rate.Year == natural_disaster_death.Year), 
+                (gdp_ppp_per_capita.Entity == natural_disaster_death.Entity) & (gdp_ppp_per_capita.Year == natural_disaster_death.Year),
                 'left')\
             .drop(natural_disaster_death.Entity, natural_disaster_death.Year, natural_disaster_death.Code)\
             .join(
                 internet_penetration, 
-                (crime_rate.Entity == internet_penetration.Entity) & (crime_rate.Year == internet_penetration.Year), 
+                (gdp_ppp_per_capita.Entity == internet_penetration.Entity) & (gdp_ppp_per_capita.Year == internet_penetration.Year),
                 'left')\
             .drop(internet_penetration.Entity, internet_penetration.Year, internet_penetration.Code)\
-            # .join(disease_death_year, (crime_rate.Entity == disease_death_year.name) & (crime_rate.Year == disease_death_year.name), 'left')\
-            # .drop(disease_death_year.Entity, disease_death_year.Year, disease_death_year.Code)
-
+            .join(
+                disease_death_year, 
+                (gdp_ppp_per_capita.Entity == disease_death_year.name) & (gdp_ppp_per_capita.Year == disease_death_year.year),
+                'left')\
+            .drop(disease_death_year.name, disease_death_year.year)
 
     tourism_year = intl_tourist_spending\
             .join(
@@ -155,8 +160,7 @@ def clean_data_tables():
                 (international_tourist_trips.Entity == intl_tourist_spending.Entity) & \
                     (international_tourist_trips.Year == intl_tourist_spending.Year), 
                 'left')\
-            .drop(inflation_rate.Entity, inflation_rate.Year, inflation_rate.Code)
-
+            .drop(international_tourist_trips.Entity, international_tourist_trips.Year, international_tourist_trips.Code)
 
     # Rename columns
     climate_names = {
@@ -174,6 +178,9 @@ def clean_data_tables():
         climate_year = climate_year.withColumnRenamed(old_col, new_col)
 
     health_econ_names = {
+        'Entity': 'name',
+        'Code': 'country_code',
+        'Year': 'year',
         'ny_gdp_pcap_pp_kd': 'gdp_ppp_per_capita', 
         'ny_gdp_pcap_kd': 'nominal_gdp_ppp_per_capita',
         'fp_cpi_totl_zg': 'inflation_rate_cpi_based', 
@@ -181,10 +188,42 @@ def clean_data_tables():
         'death_count__age_group_allages__sex_both_sexes__cause_natural_disasters': 'natural_disaster_deaths',
     }
 
-    for old_col, new_col in health_econ_year.items():
+    for old_col, new_col in health_econ_names.items():
         health_econ_year = health_econ_year.withColumnRenamed(old_col, new_col)
 
     tourism_year = tourism_year.withColumnRenamed('outbound_exp_us_cpi_adjust', 'intl_tourist_spending')
+
+    # The data is grouped into following tables:
+    # Climate by year (Includes aggregates)
+    # Health and Economy by year (Includes aggregates)
+    # Tourism by year
+    # Monthly rainfall (Not aggregated)
+    # Disease death (Not aggregated)
+    # Infrastructure
+
+    print("\nClimate by Year Table:")
+    climate_year.show(5)
+    climate_year.describe()
+
+    print("\nHealth and Economy by Year Table:")
+    health_econ_year.show(5)
+    health_econ_year.describe()
+
+    print("\nTourism Stats by Year Table:")
+    tourism_year.show(5)
+    tourism_year.describe()
+
+    print("\nAverage Monthly Surface Temperate by Year Table:")
+    average_monthly_surface_temp_renamed.show(5)
+    average_monthly_surface_temp_renamed.describe()
+
+    print("\nDisease Death by Year Table:")
+    disease_death_replaced.show(5)
+    disease_death_replaced.describe()
+
+    print("\nInfrastructure and Heritage Sites Table:")
+    cultural.show(20)
+    cultural.describe()
 
     # Load all files
     # files = GCSFileSystem().ls(f"gs://{bucket}/")
