75375b6154401c9564ba1b4c1d4484fd373b4ac3
pavobravo11 <gustavoa1011@gmail.com>
2024-12-17 17:42:43 -0700

    fixed the issues in the pyspark jobs, no longer have .csv at the end of the exported files

--- airflow-server/src/pySpark/aggregate_tables.py
+++ airflow-server/src/pySpark/aggregate_tables.py
@@ -26,35 +26,36 @@ def aggregate_data_tables():
     # Reading all data from bucket
 
     # Read the data from our wolrd in data as it all has the same schema
-    annual_co2 = spark.read.csv(f"gs://{bucket}/annual_co2_emissions", header=True, inferSchema=True)
-    annual_ghe = spark.read.csv(f"gs://{bucket}/annual_ghe", header=True, inferSchema=True)
-    annual_deforest = spark.read.csv(f"gs://{bucket}/annual_deforest", header=True, inferSchema=True)
-    tree_cover_loss_wildfires = spark.read.csv(f"gs://{bucket}/tree_cover_loss_wildfires", header=True, inferSchema=True)
-    energy_consumption = spark.read.csv(f"gs://{bucket}/energy_consumption", header=True, inferSchema=True)
-    average_precipitation = spark.read.csv(f"gs://{bucket}/average_precipitation", header=True, inferSchema=True)
-    gdp_ppp_per_capita = spark.read.csv(f"gs://{bucket}/gdp_ppp_per_capita", header=True, inferSchema=True)
-    gdp_nominal_per_capita = spark.read.csv(f"gs://{bucket}/gdp_nominal_per_capita", header=True, inferSchema=True)
-    inflation_rate = spark.read.csv(f"gs://{bucket}/inflation_rate", header=True, inferSchema=True)
-    crime_rate = spark.read.csv(f"gs://{bucket}/crime_rate", header=True, inferSchema=True)
-    intl_tourist_spending = spark.read.csv(f"gs://{bucket}/intl_tourist_spending", header=True, inferSchema=True)
-    natural_disaster_death = spark.read.csv(f"gs://{bucket}/natural_disaster_death", header=True, inferSchema=True)
-    international_tourist_trips = spark.read.csv(f"gs://{bucket}/international_tourist_trips", header=True, inferSchema=True)
+    annual_co2 = spark.read.csv(f"gs://{bucket}/annual_co2_emissions/", header=True, inferSchema=True)
+    annual_ghe = spark.read.csv(f"gs://{bucket}/annual_ghe/", header=True, inferSchema=True)
+    annual_deforest = spark.read.csv(f"gs://{bucket}/annual_deforest/", header=True, inferSchema=True)
+    tree_cover_loss_wildfires = spark.read.csv(f"gs://{bucket}/tree_cover_loss_wildfires/", header=True, inferSchema=True)
+    energy_consumption = spark.read.csv(f"gs://{bucket}/energy_consumption/", header=True, inferSchema=True)
+    average_precipitation = spark.read.csv(f"gs://{bucket}/average_precipitation/", header=True, inferSchema=True)
+    gdp_ppp_per_capita = spark.read.csv(f"gs://{bucket}/gdp_ppp_per_capita/", header=True, inferSchema=True)
+    gdp_nominal_per_capita = spark.read.csv(f"gs://{bucket}/gdp_nominal_per_capita/", header=True, inferSchema=True)
+    inflation_rate = spark.read.csv(f"gs://{bucket}/inflation_rate/", header=True, inferSchema=True)
+    crime_rate = spark.read.csv(f"gs://{bucket}/crime_rate/", header=True, inferSchema=True)
+    intl_tourist_spending = spark.read.csv(f"gs://{bucket}/intl_tourist_spending/", header=True, inferSchema=True)
+    natural_disaster_death = spark.read.csv(f"gs://{bucket}/natural_disaster_death/", header=True, inferSchema=True)
+    international_tourist_trips = spark.read.csv(f"gs://{bucket}/international_tourist_trips/", header=True, inferSchema=True)
 
-    disease_death = spark.read.csv(f"gs://{bucket}/disease_death.csv", header=True, inferSchema=True)
+    disease_death = spark.read.csv(f"gs://{bucket}/disease_death/", header=True, inferSchema=True)
     disease_death_renamed = disease_death.withColumnRenamed('location_name', 'name')
     disease_death_replaced = disease_death_renamed.withColumn(
         "name",
         when(col("name") == "United States of America", "United States").otherwise(col("name")))
 
+
     # Aggregated disease_death
     disease_death_year = disease_death_replaced.groupBy(['name', 'year']).agg(sum('val').alias('disease_death_val'))
 
-    internet_penetration = spark.read.csv(f"gs://{bucket}/internet_penetration_rate.csv", header=True, inferSchema=True)
+    internet_penetration = spark.read.csv(f"gs://{bucket}/internet_penetration_rate/", header=True, inferSchema=True)
     internet_penetration = internet_penetration\
         .withColumnRenamed('it_net_user_zs', 'internet_user_score')\
     
     # Monthly surface temp data
-    average_monthly_surface_temp_full = spark.read.csv(f"gs://{bucket}/average_monthly_surface_temp.csv", header=True, inferSchema=True)
+    average_monthly_surface_temp_full = spark.read.csv(f"gs://{bucket}/average_monthly_surface_temp/", header=True, inferSchema=True)
     average_monthly_surface_temp = average_monthly_surface_temp_full.select('Entity', 'Code', 'year', 'temperature_2m')
     average_monthly_surface_temp_renamed = average_monthly_surface_temp\
         .withColumnRenamed('Entity', 'name')\
@@ -71,13 +72,13 @@ def aggregate_data_tables():
     )
 
     # Data that is not yearly
-    world_heritage_sites = spark.read.csv(f"gs://{bucket}/unesco_whs_by_country.csv", header=True, inferSchema=True)
+    world_heritage_sites = spark.read.csv(f"gs://{bucket}/unesco_whs_by_country/", header=True, inferSchema=True)
     world_heritage_sites_renamed = world_heritage_sites\
         .withColumnRenamed('Total sites', 'heritage_site_count')\
         .withColumnRenamed('Country', 'UNESCO_Country')
     world_heritage_sites_selected = world_heritage_sites_renamed.select('heritage_site_count', 'UNESCO_Country')
 
-    infrastructure_raw = spark.read.csv(f"gs://{bucket}/infrastructure.csv", header=True, inferSchema=True)
+    infrastructure_raw = spark.read.csv(f"gs://{bucket}/infrastructure/", header=True, inferSchema=True)
     infrastructure_selected = infrastructure_raw.select('Country', 'Overall Infrastructure Score', 'Basic Infrastructure Score', \
         'Technological Infrastructure Score', 'Scientific Infrastructure Score', 'Health and Environment Score', 'Education Score')
 
